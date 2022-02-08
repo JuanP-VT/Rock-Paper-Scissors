559e958b6fa2e5073c1d6ec67481049bfac53dac
PB-vt <cv.juanp@gmail.com>
2022-02-08 16:19:50 -0700

    Added Overall logic to the game

--- javascript.js
+++ javascript.js
@@ -1,6 +1,67 @@
-let = playerScore;
-let = pcScore;
-let = playerChoice;
-let = pcChoice;
+/* Pc and Players chooses  */
 
-playerChoice = prompt("Choose Rock Paper Scissors") ;
+
+let pcChoice = 2 /*Math.floor(Math.random() * 3);*/
+if (pcChoice == 0) {
+    pcChoice = 'rock'
+} else if (pcChoice == 1) {
+    pcChoice = 'paper'
+} else if (pcChoice == 2) {
+    pcChoice = 'scissors'
+}
+
+let playerChoice = prompt("Choose Rock Paper Scissors").toLowerCase();;
+console.log("Player Choose" + " " + playerChoice);
+console.log("PC Choose" + " " + pcChoice);
+
+/* Overall Logic */
+let playerScore = 0;
+let pcScore = 0;
+let result = 0;
+
+if (playerChoice == 'rock' | playerChoice == 'paper' | playerChoice == 'scissors') {
+    if (playerChoice == 'rock'){
+        if (pcChoice == 'rock'){
+            result = 'draw'
+        }
+        if (pcChoice == 'paper'){
+            result = 'lose'
+        }
+        if (pcChoice == 'scissors'){
+            result = 'win'
+        }
+        
+    }
+    
+    if (playerChoice == 'paper'){
+        if (pcChoice == 'rock'){
+            result = 'wins'
+        }
+        if (pcChoice == 'paper'){
+            result = 'draw'
+        }
+        if (pcChoice == 'scissors'){
+            result = 'lose'
+        }
+        
+    }
+
+    if (playerChoice == 'scissors'){
+        if (pcChoice == 'rock'){
+            result = 'lose'
+        }
+        if (pcChoice == 'paper'){
+            result = 'win'
+        }
+        if (pcChoice == 'scissors'){
+            result = 'draw'
+        }
+        
+    }
+    console.log(result);
+
+  
+
+} else {
+    alert('Please Choose Correctly');
+}

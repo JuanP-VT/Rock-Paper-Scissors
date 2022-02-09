6de4f3b8ea24ace0858c34b3a552fc07197b2f80
PB-vt <cv.juanp@gmail.com>
2022-02-09 12:36:34 -0700

    Remade project to add functions support

--- javascript.js
+++ javascript.js
@@ -1,114 +1,30 @@
-
-
-let playerScore = 0;
-let pcScore = 0;
-let result = 0;
-let winner;
-
-for (let i = 1; i < 25; i++) {
-/* Pc and Players chooses  */
-
-let pcChoice = Math.floor(Math.random() * 3);
-if (pcChoice == 0) {
-    pcChoice = 'rock'
-} else if (pcChoice == 1) {
-    pcChoice = 'paper'
-} else if (pcChoice == 2) {
-    pcChoice = 'scissors'
-}
-let playerChoice = prompt("Choose Rock Paper Scissors").toLowerCase();;
-
-/* First print dialog */
-
-console.log("Game" + ' ' +i)
-console.log("Player Choose" + " " + playerChoice);
-console.log("PC Choose" + " " + pcChoice);
-
-
-/* Overall game logic */
-
-    if (playerChoice == 'rock' | playerChoice == 'paper' | playerChoice == 'scissors') {
-        if (playerChoice == 'rock'){
-            if (pcChoice == 'rock'){
-                result = 'draw'
-            }
-            if (pcChoice == 'paper'){
-                result = 'lose'
-            }
-            if (pcChoice == 'scissors'){
-                result = 'win'
-            }
-            
-        }
-        
-        if (playerChoice == 'paper'){
-            if (pcChoice == 'rock'){
-                result = 'win'
-            }
-            if (pcChoice == 'paper'){
-                result = 'draw'
-            }
-            if (pcChoice == 'scissors'){
-                result = 'lose'
-            }
-            
-        }
-    
-        if (playerChoice == 'scissors'){
-            if (pcChoice == 'rock'){
-                result = 'lose'
-            }
-            if (pcChoice == 'paper'){
-                result = 'win'
-            }
-            if (pcChoice == 'scissors'){
-                result = 'draw'
-            }
-            
-        }
-
-        /* Display results and move variables*/
-        console.log(result);
-    
-      
-    if (result == 'win'){
-        playerScore ++;
+/* The computer will pick one at random and display it*/
+function pcPlay(){
+    pcChoice = Math.floor(Math.random() * 3);
+    if (pcChoice ==0){
+        pcChoice = 'rock'
     }
-    if (result == 'lose')
-        pcScore ++;
-    console.log("Pc Score :"+ " " + pcScore);
-    console.log("Player Score :"+ " " + playerScore);
-    console.log  (" ");
-
-
-    
-    } else {
-        alert('Please Choose Correctly');
-        console.log (" Invalid output , game will be invalidated")
-        console.log  (" ");
-
+    else if(pcChoice == 1) {
+        pcChoice = 'paper'
     }
-if (playerScore == 5){
-    winner = 'Player';
-}
-if (pcScore == 5){
-    winner = 'PC';
-
-}
-
-/* Loop terminator */
-
-
-if (playerScore ==5 | pcScore ==5) {
-    console.log ("FINISH!!" + "   " + winner + " " + "HAS WON!!!" )
-    i = 25
-    
-
-}    
-
+    else if (pcChoice == 2){
+        pcChoice = 'scissors'
+    }
+    console.log(pcChoice)
+    return pcChoice;
 }
 
+function playerPlay(){
+    playerChoice = prompt("pick Rock  -  Paper  -  Scissors").toLowerCase();
+    if (playerChoice == 'rock'| playerChoice == 'paper' | playerChoice == 'scissors'){
+        console.log("Player picks" + " " + playerChoice)
+    }
+    else{
+        alert(" Please pick Rock, Paper or Scissors")
+        console.log("Invalid input, game will be invalidated");
+        console.log(" ");
+    }
+}
 
 
-
-
+playerPlay();

c687da60329e19591ef4294db01c4416689f5510
PB-vt <cv.juanp@gmail.com>
2022-02-08 20:41:16 -0700

    Added loop and win condition, game is working

--- javascript.js
+++ javascript.js
@@ -1,7 +1,14 @@
+
+
+let playerScore = 0;
+let pcScore = 0;
+let result = 0;
+let winner;
+
+for (let i = 1; i < 25; i++) {
 /* Pc and Players chooses  */
 
-
-let pcChoice = 2 /*Math.floor(Math.random() * 3);*/
+let pcChoice = Math.floor(Math.random() * 3);
 if (pcChoice == 0) {
     pcChoice = 'rock'
 } else if (pcChoice == 1) {
@@ -9,66 +16,99 @@ if (pcChoice == 0) {
 } else if (pcChoice == 2) {
     pcChoice = 'scissors'
 }
-
 let playerChoice = prompt("Choose Rock Paper Scissors").toLowerCase();;
+
+/* First print dialog */
+
+console.log("Game" + ' ' +i)
 console.log("Player Choose" + " " + playerChoice);
 console.log("PC Choose" + " " + pcChoice);
 
-/* Overall Logic */
-let playerScore = 0;
-let pcScore = 0;
-let result = 0;
 
-if (playerChoice == 'rock' | playerChoice == 'paper' | playerChoice == 'scissors') {
-    if (playerChoice == 'rock'){
-        if (pcChoice == 'rock'){
-            result = 'draw'
-        }
-        if (pcChoice == 'paper'){
-            result = 'lose'
-        }
-        if (pcChoice == 'scissors'){
-            result = 'win'
+/* Overall game logic */
+
+    if (playerChoice == 'rock' | playerChoice == 'paper' | playerChoice == 'scissors') {
+        if (playerChoice == 'rock'){
+            if (pcChoice == 'rock'){
+                result = 'draw'
+            }
+            if (pcChoice == 'paper'){
+                result = 'lose'
+            }
+            if (pcChoice == 'scissors'){
+                result = 'win'
+            }
+            
         }
         
-    }
+        if (playerChoice == 'paper'){
+            if (pcChoice == 'rock'){
+                result = 'win'
+            }
+            if (pcChoice == 'paper'){
+                result = 'draw'
+            }
+            if (pcChoice == 'scissors'){
+                result = 'lose'
+            }
+            
+        }
     
-    if (playerChoice == 'paper'){
-        if (pcChoice == 'rock'){
-            result = 'wins'
+        if (playerChoice == 'scissors'){
+            if (pcChoice == 'rock'){
+                result = 'lose'
+            }
+            if (pcChoice == 'paper'){
+                result = 'win'
+            }
+            if (pcChoice == 'scissors'){
+                result = 'draw'
+            }
+            
         }
-        if (pcChoice == 'paper'){
-            result = 'draw'
-        }
-        if (pcChoice == 'scissors'){
-            result = 'lose'
-        }
-        
+
+        /* Display results and move variables*/
+        console.log(result);
+    
+      
+    if (result == 'win'){
+        playerScore ++;
     }
+    if (result == 'lose')
+        pcScore ++;
+    console.log("Pc Score :"+ " " + pcScore);
+    console.log("Player Score :"+ " " + playerScore);
+    console.log  (" ");
 
-    if (playerChoice == 'scissors'){
-        if (pcChoice == 'rock'){
-            result = 'lose'
-        }
-        if (pcChoice == 'paper'){
-            result = 'win'
-        }
-        if (pcChoice == 'scissors'){
-            result = 'draw'
-        }
-        
+
+    
+    } else {
+        alert('Please Choose Correctly');
+        console.log (" Invalid output , game will be invalidated")
+        console.log  (" ");
+
     }
-    console.log(result);
-
-  
-if (result == 'win'){
-    playerScore ++;
+if (playerScore == 5){
+    winner = 'Player';
 }
-if (result == 'lose')
-    pcScore ++;
-console.log("Pc Score :"+ " " + pcScore);
-console.log("Player Score :"+ " " + playerScore);
+if (pcScore == 5){
+    winner = 'PC';
 
-} else {
-    alert('Please Choose Correctly');
 }
+
+/* Loop terminator */
+
+
+if (playerScore ==5 | pcScore ==5) {
+    console.log ("FINISH!!" + "   " + winner + " " + "HAS WON!!!" )
+    i = 25
+    
+
+}    
+
+}
+
+
+
+
+

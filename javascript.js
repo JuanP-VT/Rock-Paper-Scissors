dbec57815e8cc719b862eb9c411c7a2b9442b1e0
PB-vt <cv.juanp@gmail.com>
2022-02-15 12:15:16 -0700

    fixed bug where profileborder would not change to default after match ended

--- javascript.js
+++ javascript.js
@@ -71,8 +71,37 @@ function trackScore(){
 
     }
 }
+
+//Change border profile color.
+function borderChange(Result){
+    const playerProfile = document.querySelector('#playerimgprofile');
+    const pcProfile = document.querySelector('#pcimgprofile');
+    if (Result !== 'draw'){
+    playerProfile.classList.remove('winner');
+    playerProfile.classList.remove('loser');
+    pcProfile.classList.remove('loser');
+    pcProfile.classList.remove('winner');
+    if (Result == 'win'){
+        playerProfile.classList.add('winner');
+        pcProfile.classList.add('loser');
+
+    }
+    if (Result == 'lose'){
+        playerProfile.classList.add('loser');
+        pcProfile.classList.add('winner');
+    
+    }
+    }
+}
+//Check for a winner.
 function winner(){
+    const playerProfile = document.querySelector('#playerimgprofile');
+    const pcProfile = document.querySelector('#pcimgprofile');
     if (playScore == 5 ){
+        playerProfile.classList.remove('winner');
+        playerProfile.classList.remove('loser');
+        pcProfile.classList.remove('loser');
+        pcProfile.classList.remove('winner');
         const resultText = document.querySelector('#result');
         resultText.textContent = '!!!GAME OVER PLAYER WON!!!';
         playScore = 0 , pcScore = 0;
@@ -83,6 +112,10 @@ function winner(){
         return playScore = 0 , pcScore = 0;;
         }
     else if ( pcScore == 5){
+        playerProfile.classList.remove('winner');
+        playerProfile.classList.remove('loser');
+        pcProfile.classList.remove('loser');
+        pcProfile.classList.remove('winner');
         const resultText = document.querySelector('#result');
         resultText.textContent = '!!!GAME OVER PC WON!!!';
         playScore = 0 , pcScore = 0;
@@ -103,15 +136,32 @@ rock.addEventListener('click',() => {
     playerMove.textContent = 'Player pick ' + playerChoice;
     playGame(playerChoice , pcChoice);
     trackScore(endResult);
+    borderChange(endResult);
     winner()
 });
 
-
-
 const paper = document.querySelector('#paper');
+paper.addEventListener('click',() => {
+    pcPlay();
+    let playerChoice = 'paper';
+    const playerMove = document.querySelector('#playermove')
+    playerMove.textContent = 'Player pick ' + playerChoice;
+    playGame(playerChoice , pcChoice);
+    trackScore(endResult);
+    borderChange(endResult);
+    winner();
+});
+
 const scissors = document.querySelector('#scissors');
-
-
-
+scissors.addEventListener('click',() => {
+    pcPlay();
+    let playerChoice = 'scissors';
+    const playerMove = document.querySelector('#playermove')
+    playerMove.textContent = 'Player pick ' + playerChoice;
+    playGame(playerChoice , pcChoice);
+    trackScore(endResult);
+    borderChange(endResult);
+    winner();
+});
 
 
